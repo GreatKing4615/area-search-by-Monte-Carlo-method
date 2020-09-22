02c88d3435025768d4b0db70989823a807b3d2d6
GreatKing4615 <59148289+GreatKing4615@users.noreply.github.com>
2020-09-22 21:38:56 +0500

    Save local repo

---
+++ Monte-Carlo/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace Monte_Carlo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.random_textbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.number_coords = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.x_coord = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.y_coord = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.random_textbox);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.tabControl1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.splitter1);
+            this.panel1.Controls.Add(this.chart1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 313);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "count=";
+            // 
+            // random_textbox
+            // 
+            this.random_textbox.Location = new System.Drawing.Point(62, 310);
+            this.random_textbox.Name = "random_textbox";
+            this.random_textbox.Size = new System.Drawing.Size(100, 20);
+            this.random_textbox.TabIndex = 29;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 336);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "R=";
+            this.label2.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 336);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 27;
+            this.textBox1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 23);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "Расчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(8, 379);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 23);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "Нарисовать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(3, 74);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(166, 193);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(158, 167);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Вход";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.number_coords,
+            this.x_coord,
+            this.y_coord});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(152, 161);
+            this.dataGridView1.TabIndex = 27;
+            // 
+            // number_coords
+            // 
+            this.number_coords.HeaderText = "№";
+            this.number_coords.Name = "number_coords";
+            // 
+            // x_coord
+            // 
+            this.x_coord.HeaderText = "X";
+            this.x_coord.Name = "x_coord";
+            // 
+            // y_coord
+            // 
+            this.y_coord.HeaderText = "Y";
+            this.y_coord.Name = "y_coord";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(158, 167);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Выход";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Выберите фигуру";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Полигон",
+            "Круг"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(157, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(175, 450);
+            this.splitter1.TabIndex = 0;
+            this.splitter1.TabStop = false;
+            this.splitter1.Visible = false;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Right;
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(175, 0);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Series2";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Series3";
+            this.chart1.Series.Add(series4);
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(625, 450);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(8, 269);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(154, 35);
+            this.textBox2.TabIndex = 31;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn number_coords;
+        private System.Windows.Forms.DataGridViewTextBoxColumn x_coord;
+        private System.Windows.Forms.DataGridViewTextBoxColumn y_coord;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox random_textbox;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Monte-Carlo/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Monte_Carlo
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        int num = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+     
+
+        private void print_Figure()
+        {
+
+
+         /*   chart1.ChartAreas[0].AxisX.Minimum = -20; // настройка минимума и максимума оси X
+            chart1.ChartAreas[0].AxisX.Maximum = 20;
+            chart1.ChartAreas[0].AxisY.Minimum = -20;
+            chart1.ChartAreas[0].AxisY.Maximum = 20;*/
+            chart1.Series[0].Points.Clear();
+            chart1.Series[0].MarkerStyle = MarkerStyle.Circle;
+            chart1.Series[0].ChartType = SeriesChartType.Line; // Тип графика - линия
+            chart1.Series[0].BorderWidth = 5;
+
+            List<List<int>> data_about_figure = input_data();
+           switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    {
+                       
+                        print_polygon(data_about_figure); 
+                        break;
+                    }
+                case 1:
+                    {
+
+                        if (textBox1.Text == "")
+                        {
+                            MessageBox.Show("Введите радиус");
+                        }
+                        else
+                        {
+                            chart1.Series[0].ChartType = SeriesChartType.Point;
+                            print_circle(Convert.ToInt32(textBox1.Text));
+                        }
+                        break;
+                    }            
+                default: break;
+            }
+
+
+        }
+
+        private void print_polygon(List<List<int>> data_about_figure)
+        {
+            int[] temp = new int[2];
+            
+            chart1.Series[0].ChartType = SeriesChartType.Line; // Тип графика - линия
+            foreach (List<int> coords in data_about_figure)
+            {
+                foreach (int x in coords)
+                {
+                    temp = coords.ToArray();
+                    chart1.Series[0].Points.AddXY(temp[0], temp[1]);
+                }
+                
+            }
+            int[] finish = data_about_figure[0].ToArray();
+            chart1.Series[0].Points.AddXY(finish[0], finish[1]);
+        }
+
+        //**РИСУНОК КРУГА**/
+        private void print_circle( int R)
+        {            
+            for (double i = -R; i <= R; i+=0.01)
+            {
+                chart1.Series[0].Points.AddXY(i, Math.Sqrt((Math.Pow(R, 2) - Math.Pow(i, 2))));
+                chart1.Series[0].Points.AddXY(i, -Math.Sqrt((Math.Pow(R, 2) - Math.Pow(i, 2))));
+            }
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<List<int>> data_about_figure = input_data();
+
+            Monte_Carlo(data_about_figure);
+        }
+
+        private void Monte_Carlo(List<List<int>> data_about_figure)
+        {
+
+            int [] coords =print_square(data_about_figure);
+            //coords[0]=дальний
+            //coords[1]=высший
+            //coords[2]=ближний
+            //coords[3]=нижний
+            
+                      
+            if (random_textbox.Text == "")
+            {
+                MessageBox.Show("Введите количество точек");
+            }
+            else {
+                int random_count = Convert.ToInt32(random_textbox.Text);
+                // List<List<int>> random_point = new List<List<int>>();
+                chart1.Series[2].BorderWidth = 10;
+                chart1.Series[2].ChartType = SeriesChartType.Point;
+                chart1.Series[2].Color = Color.DarkBlue;
+                double[] temp_point = new double[2];
+                
+                for (int i = 0; i < random_count; i++)
+                {
+                    temp_point[0]= GetRandomNumber(coords[2], coords[0]);
+                    temp_point[1] = GetRandomNumber(coords[3], coords[1]);
+                    textBox2.Text = Convert.ToString(temp_point[0]) + "-" + temp_point[1].ToString();
+                    chart1.Series[2].Points.AddXY(temp_point[0], temp_point[1]);
+                }
+            }
+        }
+
+        static double GetRandomNumber(double minimum, double maximum)
+        {
+            Random random = new Random();
+            return random.NextDouble() * (maximum - minimum) + minimum;
+        }
+
+        private int [] print_square(List<List<int>> data_about_figure)
+        {
+            List<int> x_coord = new List<int>();
+            List<int> y_coord = new List<int>();
+            int[] temp = new int[2];
+            foreach(List<int> coords in data_about_figure)
+            {
+                foreach(int x in coords)
+                {
+                    temp = coords.ToArray();
+                    x_coord.Add(temp[0]);
+                    y_coord.Add(temp[1]);
+                }
+            }
+            x_coord.Sort();
+            y_coord.Sort();
+
+            chart1.Series[1].BorderWidth = 5;
+            chart1.Series[1].ChartType = SeriesChartType.Line; // Тип графика - линия
+            chart1.Series[1].Color=Color.Red; // Тип графика - линия
+            chart1.Series[1].Points.AddXY(x_coord[0],y_coord[0]);
+            chart1.Series[1].Points.AddXY(x_coord[x_coord.Count-1],y_coord[0]);
+            chart1.Series[1].Points.AddXY(x_coord[x_coord.Count-1],y_coord[y_coord.Count-1]);
+            chart1.Series[1].Points.AddXY(x_coord[0],y_coord[y_coord.Count-1]);
+            chart1.Series[1].Points.AddXY(x_coord[0],y_coord[0]);
+
+            //передаю координаты максимальных отклонений фигуры
+            int[] answer = new int[4];
+            answer[0] = x_coord[x_coord.Count - 1];
+            answer[1] = y_coord[y_coord.Count - 1];
+            answer[2] = y_coord[0];
+            answer[3] = y_coord[0];
+            return answer;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            input_data();
+            print_Figure();
+            
+        }
+
+        //ВВод данных в лист
+        private List<List<int>> input_data()
+        {
+            List<List<int>> data_about_figure = new List<List<int>>();
+            for (int i = 0; i < dataGridView1.Rows.Count-1; i++)
+                data_about_figure.Add(new List<int> { Convert.ToInt32(dataGridView1.Rows[i].Cells[1].Value), Convert.ToInt32(dataGridView1.Rows[i].Cells[2].Value) });
+            return data_about_figure;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            textBox1.Visible = false;
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    {                       
+                        break;
+                    }
+                case 1:
+                    {                       
+                        label2.Visible = true;
+                        textBox1.Visible = true;                        
+                        break;
+                    }
+
+                default: break;
+            }
+        }
+
+
+      
+
+       
+    }
+}
